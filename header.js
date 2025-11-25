0f568fdf2d5253e03af40865b3f65cc3b8eb3398
valdez-jose <tu-email-de-github@example.comclear>
2025-11-25 11:58:54 -0300

    Mi primer commit

---
+++ header.js
@@ -0,0 +1,14 @@
+
+ const btn = document.getElementById("btnHamburguesa");
+  const menu = document.getElementById("menuNav");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("activo");
+  });
+
+ 
+
+
+
+
+
